11ef1b9a0f384011bfe05b91b13a18dcebdfb807
Tobias Prima <primatjahja@gmail.com>
2024-03-20 00:00:09 +0800

    feat: babel config

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,19 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import TodoList from './TodoList';
+import TodoList from './features/todos/TodoList';
 
 test('renders todo list', () => {
   render(<TodoList />);
   
-  // Check if the heading "Todo List" is rendered
   const headingElement = screen.getByRole('heading', { name: /todo list/i });
   expect(headingElement).toBeInTheDocument();
 
-  // Check if the input field for adding a new todo is rendered
   const inputElement = screen.getByLabelText(/Add to list/i);
   expect(inputElement).toBeInTheDocument();
 
-  // Check if the submit button is rendered
   const submitButton = screen.getByRole('button', { name: /submit/i });
   expect(submitButton).toBeInTheDocument();
 });
 
-// You can add more tests to simulate user interaction as follows
 
 test('allows user to type in a new todo', () => {
   render(<TodoList />);
