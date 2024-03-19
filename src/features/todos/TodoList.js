580f86be1b9a473913bcd841a151d3608f81809f
Tobias Prima <primatjahja@gmail.com>
2024-03-20 03:19:35 +0800

    feat: progress bar and input at the bottom

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -31,21 +31,23 @@ const TodoList = () => {
     }
 
     const newItemSection = 
-    <form onSubmit={handleSubmit}>
-        <label htmlFor="new-todo">Add to list</label>
-        <div className="new-todo">
-            <input 
-            type="text"
-            id="new-todo"
-            value={newTodo}
-            onChange={(e)=> setNewTodo(e.target.value)}
-            placeholder="Add New Todo"
-            />
-        </div>
-        <button className="submit">
-            <FontAwesomeIcon icon={faUpload} />
-        </button>
-    </form>
+    <div className="new-item-section">
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="new-todo">Add to list</label>
+            <div className="new-todo">
+                <input 
+                type="text"
+                id="new-todo"
+                value={newTodo}
+                onChange={(e)=> setNewTodo(e.target.value)}
+                placeholder="Add New Todo"
+                />
+            </div>
+            <button className="submit">
+                <FontAwesomeIcon icon={faUpload} />
+            </button>
+        </form>
+    </div>
 
     let content;
     if (isLoading) {
@@ -75,9 +77,30 @@ const TodoList = () => {
 
   return (
     <main>
-        <h1>Todo List</h1>
-        {newItemSection}
-        {content}
+        <header>
+            <h1>Todo List</h1>
+            <p>Add Things to do</p>
+        </header>
+        <hr />
+        <div className='progress-container'>
+            <div className="progress-label">
+                {todos ? Math.round((todos.filter(todo => todo.status).length / todos.length) * 100) : 0}%
+            </div>
+            <progress
+            className="progress-bar" 
+            value={todos ? todos.filter(todo => todo.status).length : 0} 
+            max={todos ? todos.length : 0}
+            />
+        </div>
+        
+        <div className="content">
+            {content}
+        </div>
+        <hr />
+        <div className="bottom-section">
+            <h3>Add to list</h3>
+            {newItemSection}
+        </div>
     </main>
   )
 }
