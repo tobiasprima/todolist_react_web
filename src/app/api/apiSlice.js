c693e2c04784befb01e4dc45648fb254a93d7add
Tobias Prima <primatjahja@gmail.com>
2024-03-20 23:44:02 +0800

    feat: update base query to use deployed backend

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://todolistapi-kcjj.onrender.com' }),
     tagTypes: ['Todos'],
     endpoints: (builder) => ({
         getTodos: builder.query({
