9dff536c9b381aa622bf485f9073039f9802dc51
Tobias Prima <primatjahja@gmail.com>
2024-03-20 20:24:35 +0800

    feat: improve ui

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -40,7 +40,6 @@ const TodoList = () => {
                 id="new-todo"
                 value={newTodo}
                 onChange={(e)=> setNewTodo(e.target.value)}
-                placeholder="Add New Todo"
                 />
             </div>
             <button className="submit">
@@ -98,7 +97,7 @@ const TodoList = () => {
         </div>
         <hr />
         <div className="bottom-section">
-            <h3>Add to list</h3>
+            <p>Add to list</p>
             {newItemSection}
         </div>
     </main>
